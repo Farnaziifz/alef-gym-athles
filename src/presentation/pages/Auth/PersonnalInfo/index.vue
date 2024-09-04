c39f01dbb3d18896560f89ccf79e1f533c3536e4
farnaz <ffarzipour92@gmail.com>
2024-09-04 15:23:45 +0330

    username and personal info pages

---
+++ src/presentation/pages/Auth/PersonnalInfo/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import PageHader from '@/presentation/components/shared/PageHader.vue';
+import LoginBanner from '@/assets/images/Banners/Login-Banner.png';
+import Logo from '@/assets/images/Generals/Logo.svg';
+import Button from '@/presentation/components/shared/button.vue';
+import CustomInput from '@/presentation/components/shared/CustomInput.vue';
+
+const phoneNumber = ref('');
+</script>
+
+<template>
+  <div>
+    <PageHader :img="LoginBanner" />
+    <div class="container">
+      <div class="flex flex-col items-center">
+        <img :src="Logo" class="w-24" />
+        <p class="text-white font-[dana-demi] text-2xl mt-32">اطلاعات شخصی</p>
+        <CustomInput type="number" :model-value="phoneNumber" placeholder="نام" class="mt-8" />
+        <CustomInput
+          type="number"
+          :model-value="phoneNumber"
+          placeholder="نام خانوادگی"
+          class="mt-8"
+        />
+        <Button text="ارسال" color="primary" bg-color="secondary" rounded="40px" class="mt-2" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/resorces/router/Modules/auth.config.ts
+++ src/resorces/router/Modules/auth.config.ts
@@ -1,6 +1,8 @@
 import Auth from '@/presentation/pages/Auth/index.vue';
 import Login from '@/presentation/pages/Auth/Login/index.vue';
 import OTP from '@/presentation/pages/Auth/OTP/index.vue';
+import PersonnalInfo from '@/presentation/pages/Auth/PersonnalInfo/index.vue';
+import Usernanme from '@/presentation/pages/Auth/Username/index.vue';
 export const AuthRouterConfig = {
   name: 'auth',
   path: 'auth',
@@ -16,6 +18,16 @@ export const AuthRouterConfig = {
       name: 'OTP',
       path: 'otp',
       component: OTP
+    },
+    {
+      name: 'PersonnalInfo',
+      path: 'personnalInfo',
+      component: PersonnalInfo
+    },
+    {
+      name: 'username',
+      path: 'username',
+      component: Usernanme
     }
   ]
 };
